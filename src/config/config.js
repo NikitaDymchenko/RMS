23a52c600d547ea0d9587cc0a9c01c2cdaa9bc3e
Nikita Dymchenko <133228841+NikitaDymchenko@users.noreply.github.com>
2025-04-28 17:08:56 +0700

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,8 @@
-import 'dotenv/config';
+import dotenv from 'dotenv';
 import { MongoClient } from 'mongodb';
 import { createClient } from '@clickhouse/client';
+
+dotenv.config();
 
 export const {
   MONGO_HOST,
@@ -11,23 +13,17 @@ export const {
   REPLICA_CH_HOST,
   REPLICA_CH_SECURE,
   REPLICA_CH_USER,
-  REPLICA_CH_PASSWORD,
-  VERBOSE_LOGGING = 'false'
+  REPLICA_CH_PASSWORD
 } = process.env;
 
-// Flag to control verbose debug logging
-export const VERBOSE = VERBOSE_LOGGING === 'true';
-
 export function getMongoClient() {
-  if (VERBOSE) console.debug('[config] Creating MongoClient with', MONGO_HOST, MONGO_DB);
   const uri = `mongodb://${MONGO_USER}:${encodeURIComponent(MONGO_PASS)}@${MONGO_HOST}/${MONGO_DB}?authSource=${MONGO_AUTH_SOURCE}`;
   return new MongoClient(uri);
 }
 
-function buildCHUrl(host, secure=false) {
-  const proto = secure==='true'?'https':'http';
-  if (VERBOSE) console.debug('[config] ClickHouse URL:', proto, '://', host);
-  return `${proto}://${host}`;
+function buildCHUrl(host, secure = false) {
+  const protocol = secure === 'true' ? 'https' : 'http';
+  return `${protocol}://${host}`;
 }
 
 export const clickhouseReplicaClient = createClient({
@@ -35,8 +31,3 @@ export const clickhouseReplicaClient = createClient({
   username: REPLICA_CH_USER,
   password: REPLICA_CH_PASSWORD
 });
-  logInfo(`ChangeStream start ${resume?'resume':'new'}`);
-  const cs=coll.watch([],opts);
-  cs.on('change',async c=>{logInfo(`change ${c.documentKey._id}`);try{const doc=c.fullDocument;await loadCallStatus(doc);if(!doc.active)queue.enqueue({callId:doc._id.toString()});saveResumeToken(c._id);}catch(e){logError('cs',e);}});
-  cs.on('error',e=>{logError('cs err',e);process.exit(1);});
-}
